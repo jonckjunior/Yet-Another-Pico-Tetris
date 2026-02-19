cac4ae6934a1fe1a6de245f9f7cb6af928e8f940
Jonck <jonckjuniorr@gmail.com>
2026-02-19 14:51:45 -0500

    Making the menu a bit better

--- menu.lua
+++ menu.lua
@@ -11,16 +11,23 @@ function draw_menu()
     -- fillp(0b0101010110101010)
     -- rectfill(0, 0, 127, 127, 0)
     -- fillp()
-    menu_network:draw()
+    -- menu_network:draw()
     local logo_width = 62
     local logo_height = 38
     local x = (127 - logo_width) / 2
     local y = (127 - logo_height) / 2 - 20
     local t = time()
-    local offset = sin(t * 1.2) * 2 -- speed * amplitude
+    local offset = sin(t * 0.5) * 2 -- speed * amplitude
 
+    circfill(x + logo_width / 2, y + offset + logo_height / 2, 30, 1)
+    local drk = { [0] = 0, 0, 1, 1, 2, 1, 5, 6, 2, 4, 9, 3, 1, 1, 2, 5 }
+    for i = 1, 15 do
+        pal(i, drk[i])
+    end
+    spr(16, x + 2, y + offset + 2, logo_width, logo_height)
+    pal()
     spr(16, x, y + offset, logo_width, logo_height)
-    local t = sin(time() * 4)
-    local col = (t > 0) and 7 or 6
-    print("press x to start", 30, 76, col)
+    -- local t = sin(time() * 4)
+    -- local col = (t > 0) and 7 or 6
+    -- print("press x to start", 30, 76, col)
 end
